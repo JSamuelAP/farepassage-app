dbb5121369ea4fe06ec99f09e3fa7c6d8b1eb213
JSamuel <sp4619168@gmail.com>
2023-06-06 20:14:55 -0600

    assets: Incluir iconos y fuente tipografica

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,9 +10,24 @@ module.exports = {
 			xs: "480px",
 			...defaultTheme.screens,
 		},
+		fontWeight: {
+			regular: "400",
+			bold: "700",
+		},
 		extend: {
 			colors: {
-				primary: "#FACA50",
+				primary: {
+					50: "#FEF8E4",
+					100: "#FDECBB",
+					200: "#FCE091",
+					300: "#FBD468",
+					400: "#FACA50",
+					500: "#F8C147",
+					600: "#F7B442",
+					700: "#F4A33D",
+					800: "#F2943A",
+					900: "#EF7931",
+				},
 			},
 		},
 	},
