07f86ac4687d405e263f56b28f349c10a814fc29
JSamuel <sp4619168@gmail.com>
2023-06-06 22:17:51 -0600

    feat: Componente Icon

---
+++ src/components/Icon.jsx
@@ -0,0 +1,47 @@
+import svgGoogle from "../assets/icons/google.svg";
+import svgHome from "../assets/icons/home.svg";
+import svgLogout from "../assets/icons/logout.svg";
+import svgOptions from "../assets/icons/options.svg";
+import svgSettings from "../assets/icons/settings.svg";
+
+const Icon = ({ name, size }) => {
+	let svgSource;
+	let width;
+
+	switch (name) {
+		case "google":
+			svgSource = svgGoogle;
+			break;
+		case "home":
+			svgSource = svgHome;
+			break;
+		case "logout":
+			svgSource = svgLogout;
+			break;
+		case "options":
+			svgSource = svgOptions;
+			break;
+		case "settings":
+			svgSource = svgSettings;
+			break;
+		default:
+			svgSource = svgHome;
+			break;
+	}
+
+	switch (size) {
+		case "base":
+			width = "w-4";
+			break;
+		case "2xl":
+			width = "w-6";
+			break;
+		default:
+			width = "w-4";
+			break;
+	}
+
+	return <img src={svgSource} alt={name} className={width} />;
+};
+
+export default Icon;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,16 @@
+import Icon from "../components/Icon";
+
 function Login() {
-	return <div>Login</div>;
+	return (
+		<>
+			<h1>login</h1>
+			<Icon name="google" size="base" />
+			<Icon name="home" size="2xl" />
+			<Icon name="logout" size="2xl" />
+			<Icon name="options" size="2xl" />
+			<Icon name="settings" size="base" />
+		</>
+	);
 }
 
 export default Login;
