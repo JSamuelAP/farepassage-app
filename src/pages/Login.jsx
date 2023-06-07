e28e4c19e33dad2b92b4d72b5933c8cea7939935
JSamuel <sp4619168@gmail.com>
2023-06-07 17:09:12 -0600

    feat: Crear vista de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,27 +1,14 @@
+import logo from "../assets/logos/Icon vertical.svg";
 import Button from "../components/Button";
-import Icon from "../components/Icon";
 
 function Login() {
 	return (
-		<>
-			<h1>login</h1>
-			<Icon name="home" size="2xl" />
-			<Icon name="logout" size="2xl" />
-			<Icon name="options" size="2xl" />
-			<Icon name="settings" size="base" />
-			<Button type="submit" fullWidth={true} icon="google">
-				<span>Iniciar sesión</span>
+		<div className="flex h-screen flex-col items-center justify-center gap-y-6">
+			<img src={logo} alt="FarePassage App logo" className="w-[200px]" />
+			<Button icon="google" size="big" fullWidth={true}>
+				Iniciar sesión
 			</Button>
-			<Button color="secondary" fullWidth={true} icon="home">
-				<span>Iniciar sesión</span>
-			</Button>
-			<Button size="big" icon="google">
-				<span>Iniciar sesión</span>
-			</Button>
-			<Button size="big" color="secondary">
-				<span>Iniciar sesión</span>
-			</Button>
-		</>
+		</div>
 	);
 }
 
