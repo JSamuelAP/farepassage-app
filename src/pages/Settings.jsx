81e37e2f56b23c75df66b0c81313998bd3635fbb
JSamuel <sp4619168@gmail.com>
2023-06-08 17:14:34 -0600

    feat: Pagina Settings

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,5 +1,58 @@
+import Navbar from "../components/Navbar";
+import Input from "../components/Input";
+import Button from "../components/Button";
+import Footer from "../components/Footer";
+
 function Settings() {
-	return <div>Settings</div>;
+	return (
+		<>
+			<Navbar />
+			<div className="flex min-h-[calc(100vh-57px)] flex-col">
+				<main className="mb-14  flex-1 text-neutral-800">
+					<div className="container mx-auto max-w-xl px-8">
+						<p className="mb-6 mt-16">
+							<img
+								src="https://placehold.co/24x24"
+								alt="Foto de perfil del usuario"
+								className="mr-2 inline-block w-6 rounded-full"
+							/>
+							Nombre de usuario
+						</p>
+						<form>
+							<div className="mb-4">
+								<label className="mb-1 block text-sm" for="input-saldo">
+									Saldo
+								</label>
+								<Input
+									placeholder="$ Saldo actual de tu tarjeta"
+									id="input-saldo"
+								/>
+							</div>
+							<div className="mb-6">
+								<label className="mb-1 mt-4 block" for="input-tarifa">
+									Tarifa de pasaje
+								</label>
+								<Input placeholder="$ Precio de un pasaje" id="input-tarifa" />
+							</div>
+							<div className="flex gap-x-2">
+								<div className="basis-1/2">
+									<Button color="secondary" fullWidth={true} type="reset">
+										Registrar
+									</Button>
+								</div>
+								<div className="basis-1/2">
+									<Button fullWidth={true} type={"submit"}>
+										Actualizar
+									</Button>
+								</div>
+							</div>
+						</form>
+					</div>
+				</main>
+				<Footer />
+			</div>
+		</>
+	);
 }
 
 export default Settings;
