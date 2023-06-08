6412627efbd0953ced8970fefca244e982c23bae
JSamuel <sp4619168@gmail.com>
2023-06-08 16:34:32 -0600

    feat: Pagina Home
    Se agrego la propiedad id en input para especificar su id.

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,9 @@
 import PropTypes from "prop-types";
 
-const Input = ({ placeholder }) => {
+const Input = ({ placeholder, id }) => {
 	return (
 		<input
+			id={id}
 			type="number"
 			placeholder={placeholder}
 			className="w-full rounded bg-white px-2 py-1 text-neutral-600 ring-2 ring-neutral-200 focus:outline-none focus:ring-primary-200"
@@ -13,6 +14,7 @@ const Input = ({ placeholder }) => {
 
 Input.prototype = {
 	placeholder: PropTypes.string,
+	id: PropTypes.string,
 };
 
 export default Input;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import Footer from "../components/Footer";
+import Button from "../components/Button";
 import Input from "../components/Input";
 import Navbar from "../components/Navbar";
 
@@ -6,10 +7,38 @@ function Home() {
 	return (
 		<>
 			<Navbar />
-			<div className="container mx-auto max-w-xl px-8">
-				<Input placeholder="$ Cantidad" />
+			<div className="flex min-h-[calc(100vh-57px)] flex-col">
+				<main className="mb-14  flex-1 text-neutral-800">
+					<div className="container mx-auto max-w-xl px-8">
+						<p className="mt-14">Hola Usuario, tu saldo es de:</p>
+						<p className="mt-8 text-center text-7xl text-neutral-700">
+							$<span className="font-bold text-neutral-800">100.00</span>
+						</p>
+						<p className="mb-8 mt-2 text-center">
+							<span className="font-bold">20</span> pasajes restantes
+						</p>
+						<Button size="big" fullWidth={true}>
+							Descontar un pasaje
+						</Button>
+						<form className="mt-16">
+							<label className="text-sm" for="input-recarga">
+								Registrar recarga
+							</label>
+							<div className="mt-1 flex gap-x-2">
+								<div className="basis-7/12">
+									<Input placeholder="$ Cantidad" id="input-recarga" />
+								</div>
+								<div className="basis-5/12">
+									<Button color="secondary" fullWidth={true} type="submit">
+										Registrar
+									</Button>
+								</div>
+							</div>
+						</form>
+					</div>
+				</main>
+				<Footer />
 			</div>
-			<Footer />
 		</>
 	);
 }
