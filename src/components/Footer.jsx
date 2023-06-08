fa247e0989c9ef578d979e336188cfeb73cc837c
JSamuel <sp4619168@gmail.com>
2023-06-08 14:40:13 -0600

    feat: Componente Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+	return (
+		<footer className="bg-slate-50 p-6">
+			<p className="text-center text-neutral-600">
+				&copy; 2023 FarePassageApp | Desarrollada por{" "}
+				<a
+					href="https://jsamuelap.github.io"
+					target="_blank"
+					className="hover:underline"
+				>
+					JSamuel
+				</a>
+			</p>
+		</footer>
+	);
+};
+
+export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import Footer from "../components/Footer";
 import Input from "../components/Input";
 import Navbar from "../components/Navbar";
 
@@ -8,6 +9,7 @@ function Home() {
 			<div className="container mx-auto max-w-xl px-8">
 				<Input placeholder="$ Cantidad" />
 			</div>
+			<Footer />
 		</>
 	);
 }
