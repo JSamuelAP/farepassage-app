40fc91510d40fbe996b131f0f0c0ff4bfb72fb84
JSamuel <sp4619168@gmail.com>
2023-06-13 16:55:41 -0600

    refactor: Utilizar Link en el logo del navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,14 @@
-import logo from "../assets/logos/Icon horizontal.svg";
+import { Link } from "react-router-dom";
 import DropdownMenu from "./DropdownMenu";
+import logo from "../assets/logos/Icon horizontal.svg";
 
 const Navbar = () => {
 	return (
 		<nav className=" py-4 shadow-sm">
 			<div className="container relative mx-auto flex max-w-xl justify-between px-8">
-				<a href="/">
+				<Link to="/">
 					<img src={logo} alt="FarePassage App logo" className="h-6" />
-				</a>
+				</Link>
 				<DropdownMenu />
 			</div>
 		</nav>
